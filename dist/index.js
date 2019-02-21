f25dabcdcf6618a62cebc945ada1eca0426aba70
maxime <m.dezette@gmail.com>
2019-02-21 16:21:32 +0100

    separation entrainement/prediction

--- dist/index.js
+++ dist/index.js
@@ -1,86 +1,13 @@
-
-
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http:// www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ==============================================================================
- */
-
-// This tiny example illustrates how little code is necessary build /
-// train / predict from a model in TensorFlow.js.  Edit this code
-// and refresh the index.html to quickly explore the API.
-
-// Tiny TFJS train / predict example.
-
 var model;
 
 
 async function run() {
 
+  // On cache le formulaire pendant l'apprentissage
+  $( "#irisForm" ).toggle();
 
-var sepalLength = parseFloat(document.getElementById("sepal_length").value);
-var sepalWidth = parseFloat(document.getElementById("sepal_width").value);
-var petalLength = parseFloat(document.getElementById("petal_length").value);
-var petalWidth = parseFloat(document.getElementById("petal_width").value);
-
-
-var irisTesting = [
-  {
-    "sepal_length": 5.7,
-    "sepal_width": 2.9,
-    "petal_length": 4.2,
-    "petal_width": 1.3,
-   
-  },
-  {
-    "sepal_length": 5.4,
-    "sepal_width": 3.9,
-    "petal_length": 1.7,
-    "petal_width": 0.4,
-    "species": "setosa"
-  },
-  {
-    "sepal_length": 5.9,
-    "sepal_width": 3,
-    "petal_length": 5.1,
-    "petal_width": 1.8,
-    "species": "virginica"
-  },
-  {
-    "sepal_length": 5.7,
-    "sepal_width": 2.9,
-    "petal_length": 4.2,
-    "petal_width": 1.3,
-    "species": "versicolor"
-  }
-  
-];
-    
-  
-
-
-  irisTesting[0]['sepal_length'] = sepalLength;
-  irisTesting[0]['sepal_width'] = sepalWidth;
-  irisTesting[0]['petal_length'] = petalLength;
-  irisTesting[0]['petal_width'] = petalWidth;
-
-  console.log(irisTesting);
- 
-// Données pour l'entrainement
- var iris= [
-    {
+  // Données pour l'entrainement
+  var iris = [{
       "sepal_length": 5.1,
       "sepal_width": 3.5,
       "petal_length": 1.4,
@@ -1110,89 +1037,141 @@ var irisTesting = [
       "species": "virginica"
     }
   ];
-  
 
-   model = tf.sequential();
-  
+
+  model = tf.sequential();
+
   const trainingData = tf.tensor2d(iris.map(item => [
-      item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
+    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
   ]))
 
   //On définie les sorties possibles
   const outputData = tf.tensor2d(iris.map(item => [
-      item.species === "setosa" ? 1 : 0,
-      item.species === "virginica" ? 1 : 0,
-      item.species === "versicolor" ? 1 : 0,
+    item.species === "setosa" ? 1 : 0,
+    item.species === "virginica" ? 1 : 0,
+    item.species === "versicolor" ? 1 : 0,
   ]))
 
 
-  const testingData = tf.tensor2d(irisTesting.map(item => [
-      item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
-  ]))
 
 
-// Les entrées
+
+  // Les entrées
   model.add(tf.layers.dense({
-      inputShape: [4],
-      activation: "sigmoid",
-      units: 4,
+    inputShape: [4],
+    activation: "sigmoid",
+    units: 5,
   }))
 
   // Première couche cachée
   model.add(tf.layers.dense({
-      inputShape: [5],
-      activation: "sigmoid",
-      units: 3,
+    inputShape: [5],
+    activation: "sigmoid",
+    units: 3,
   }))
 
   // Couche de sortie
   model.add(tf.layers.dense({
-      activation: "sigmoid",
-      units: 3,
+    activation: "sigmoid",
+    units: 3,
   }))
 
   // Fonction d'erreur
   model.compile({
-      loss: "meanSquaredError",
-      optimizer: tf.train.adam(.06),
+    loss: "meanSquaredError",
+    optimizer: tf.train.adam(.06),
   })
   // Entrainement du réseau
-  const startTime = Date.now()
   model.fit(trainingData, outputData, {
-          epochs: 100
-      })
-      .then((history) => {
-          
-        // Prédiction des données de test
-          model.predict(testingData).print();
-          const values = model.predict(testingData);
+      epochs: 100
+    })
+    .then((history) => {
 
-      
+      console.log('entrainement fini!');
+      $( "#irisForm" ).toggle();
+      $( "#entrainement").toggle();
 
-          values.slice([0, 0], 1).as1D().print();
-          console.log(Array.prototype.slice.call(values.slice([0, 0], 1).as1D().dataSync()));
 
-          var array = Array.prototype.slice.call(values.slice([0, 0], 1).as1D().dataSync());
+    })
 
-          console.log("setosa: " + array[0]);
-        
-          console.log("veronica: " + array[1]);
-
-          console.log("versicolor: " + array[2]);
-
-          
-        })
-        
 }
 
+function testData() {
+
+  console.log('test de l entree');
+
+  //On récupère les caractéristiques de l'iris
+  var sepalLength = parseFloat(document.getElementById("sepal_length").value);
+  var sepalWidth = parseFloat(document.getElementById("sepal_width").value);
+  var petalLength = parseFloat(document.getElementById("petal_length").value);
+  var petalWidth = parseFloat(document.getElementById("petal_width").value);
+
+
+  var irisTesting = [{
+      "sepal_length": 5.7,
+      "sepal_width": 2.9,
+      "petal_length": 4.2,
+      "petal_width": 1.3,
+
+    },
+    {
+      "sepal_length": 5.4,
+      "sepal_width": 3.9,
+      "petal_length": 1.7,
+      "petal_width": 0.4,
+      "species": "setosa"
+    },
+    {
+      "sepal_length": 5.9,
+      "sepal_width": 3,
+      "petal_length": 5.1,
+      "petal_width": 1.8,
+      "species": "virginica"
+    },
+    {
+      "sepal_length": 5.7,
+      "sepal_width": 2.9,
+      "petal_length": 4.2,
+      "petal_width": 1.3,
+      "species": "versicolor"
+    }
+
+  ];
+
+  irisTesting[0]['sepal_length'] = sepalLength;
+  irisTesting[0]['sepal_width'] = sepalWidth;
+  irisTesting[0]['petal_length'] = petalLength;
+  irisTesting[0]['petal_width'] = petalWidth;
+
+  const testingData = tf.tensor2d(irisTesting.map(item => [
+    item.sepal_length, item.sepal_width, item.petal_length, item.petal_width,
+  ]))
+
+  console.log(irisTesting);
+  // Prédiction des données de test
+  model.predict(testingData).print();
+  const values = model.predict(testingData);
 
 
 
+  values.slice([0, 0], 1).as1D().print();
+  console.log(Array.prototype.slice.call(values.slice([0, 0], 1).as1D().dataSync()));
+
+  var array = Array.prototype.slice.call(values.slice([0, 0], 1).as1D().dataSync());
+
+  console.log("setosa: " + array[0]);
+
+  console.log("virginica: " + array[1]);
+
+  console.log("versicolor: " + array[2]);
+
+}
+
+run();
 
 
 
 $('#testbtn').click(
-  function() { 
-  run();
-});
-
+  function () {
+    testData();
+  });
